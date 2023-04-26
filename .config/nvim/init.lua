7f08ea6da96603ac3799fab1629f478ece7266ee
Yoshinori Kashimura <yoshinori-kashimura@hldc.co.jp>
2023-04-26 11:44:13 +0900

    Removed print statement at nvim startup.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,3 @@
-if vim.g.vscode then
-  print("vscode")
-else
-  print("not vscode")
-end
-
 require("craftzdog.base")
 require("craftzdog.highlights")
 require("craftzdog.maps")
@@ -15,14 +9,11 @@ local is_win = has("win32") or has("win64")
 local is_wsl = has("wsl")
 
 if is_mac == 1 then
-  print("is_mac", is_mac)
   require("craftzdog.macos")
 end
 if is_win == 1 then
-  print("is_win", is_win)
   require("craftzdog.windows")
 end
 if is_wsl == 1 then
-  print("is_wsl", is_wsl)
   require("craftzdog.wsl")
 end
