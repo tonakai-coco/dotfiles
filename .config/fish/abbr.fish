746944f10d5818c701412f1f2376f10f805a1cf6
tonakai_coco <73034880+tonakai-coco@users.noreply.github.com>
2023-06-13 20:56:08 +0900

    Add fish aliases and load them into fish shell

---
+++ .config/fish/abbr.fish
@@ -0,0 +1,6 @@
+abbr -a -- gs 'git status'
+abbr -a -- fb 'fzf --preview "bat  --color=always --style=header,grid --line-range :100 {}"'
+abbr -a -- ga 'git add .'
+abbr -a -- gf 'git fetch'
+abbr -a -- gp 'git pull'
+abbr -a -- ll 'exa -l -g --icons'
---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+  zoxide init fish | source
+end
+
+# abbr.fishを読み込む
+source ~/.config/fish/abbr.fish
