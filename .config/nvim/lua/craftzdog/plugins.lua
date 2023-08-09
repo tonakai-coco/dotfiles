b474e2d5e8f9f083af7cf2b1c46bd9241b7c2fd0
tonakai_coco <73034880+tonakai-coco@users.noreply.github.com>
2023-08-10 01:08:44 +0900

    Add which-key.nvim plugin configuration

--- .config/nvim/lua/craftzdog/plugins.lua
+++ .config/nvim/lua/craftzdog/plugins.lua
@@ -99,5 +99,17 @@ if not vim.g.vscode then
 				"nvim-telescope/telescope.nvim",
 			},
 		})
+		use({
+			"folke/which-key.nvim",
+			config = function()
+				vim.o.timeout = true
+				vim.o.timeoutlen = 300
+				require("which-key").setup({
+					-- your configuration comes here
+					-- or leave it empty to use the default settings
+					-- refer to the configuration section below
+				})
+			end,
+		})
 	end)
 end
