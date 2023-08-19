f6e82f4efad427e67d91df37b9d965a135c1228c
tonakai_coco <73034880+tonakai-coco@users.noreply.github.com>
2023-08-19 12:35:07 +0900

    Update lspsaga keybindings to show line diagnostics

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -20,7 +20,7 @@ if not vim.g.vscode then
 
 	local opts = { noremap = true, silent = true }
 	vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-	-- vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_diagnostic<CR>", opts)
+	vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_line_diagnostics<CR>", opts)
 	vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
 	vim.keymap.set("n", "gd", "<Cmd>Lspsaga finder<CR>", opts)
 	vim.keymap.set("n", "gt", "<Cmd>Lspsaga goto_definition<CR>", opts)
