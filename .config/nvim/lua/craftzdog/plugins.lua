87d1178bf5401b31db3a1dbd848639c55227c43a
Yoshinori Kashimura <yoshinori-kashimura@hldc.co.jp>
2023-11-14 12:53:12 +0900

    Add solarized-osaka.nvim plugin

--- .config/nvim/lua/craftzdog/plugins.lua
+++ .config/nvim/lua/craftzdog/plugins.lua
@@ -1,121 +1,122 @@
 if not vim.g.vscode then
-	local status, packer = pcall(require, "packer")
-	if not status then
-		print("Packer is not installed")
-		return
-	end
+    local status, packer = pcall(require, "packer")
+    if not status then
+        print("Packer is not installed")
+        return
+    end
 
-	vim.cmd([[packadd packer.nvim]])
+    vim.cmd([[packadd packer.nvim]])
 
-	packer.startup(function(use)
-		use("wbthomason/packer.nvim")
-		use("vim-jp/vimdoc-ja")
-		use({
-			"svrana/neosolarized.nvim",
-			requires = { "tjdevries/colorbuddy.nvim" },
-		})
-		use("nvim-lualine/lualine.nvim") -- Statusline
-		use("nvim-lua/plenary.nvim") -- Common utilities
-		use("onsails/lspkind-nvim") -- vscode-like pictograms
-		use("hrsh7th/cmp-buffer") -- nvim-cmp source for buffer words
-		use("hrsh7th/cmp-nvim-lsp") -- nvim-cmp source for neovim's built-in LSP
-		use("hrsh7th/nvim-cmp") -- Completion
-		use("neovim/nvim-lspconfig") -- LSP
-		use("jose-elias-alvarez/null-ls.nvim") -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-		-- use 'MunifTanjim/prettier.nvim'
-		use("williamboman/mason.nvim")
-		use("williamboman/mason-lspconfig.nvim")
-		use("simrat39/rust-tools.nvim")
-		use("mfussenegger/nvim-dap")
-		use("rcarriga/nvim-dap-ui")
+    packer.startup(function(use)
+        use("wbthomason/packer.nvim")
+        use("vim-jp/vimdoc-ja")
+        use({
+            "svrana/neosolarized.nvim",
+            requires = { "tjdevries/colorbuddy.nvim" },
+        })
+        use("nvim-lualine/lualine.nvim")       -- Statusline
+        use("nvim-lua/plenary.nvim")           -- Common utilities
+        use("onsails/lspkind-nvim")            -- vscode-like pictograms
+        use("hrsh7th/cmp-buffer")              -- nvim-cmp source for buffer words
+        use("hrsh7th/cmp-nvim-lsp")            -- nvim-cmp source for neovim's built-in LSP
+        use("hrsh7th/nvim-cmp")                -- Completion
+        use("neovim/nvim-lspconfig")           -- LSP
+        use("jose-elias-alvarez/null-ls.nvim") -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+        -- use 'MunifTanjim/prettier.nvim'
+        use("williamboman/mason.nvim")
+        use("williamboman/mason-lspconfig.nvim")
+        use("simrat39/rust-tools.nvim")
+        use("mfussenegger/nvim-dap")
+        use("rcarriga/nvim-dap-ui")
 
-		use("nvimdev/lspsaga.nvim") -- LSP UIs
-		use("L3MON4D3/LuaSnip")
-		use({
-			"nvim-treesitter/nvim-treesitter",
-			run = function()
-				require("nvim-treesitter.install").update({
-					with_sync = true,
-				})
-			end,
-		})
-		use("kyazdani42/nvim-web-devicons") -- File icons
-		use("nvim-telescope/telescope.nvim")
-		use("nvim-telescope/telescope-file-browser.nvim")
-		use("windwp/nvim-autopairs")
-		use("windwp/nvim-ts-autotag")
-		use({
-			"numToStr/Comment.nvim",
-			requires = { "JoosepAlviste/nvim-ts-context-commentstring" },
-		})
-		use("norcalli/nvim-colorizer.lua")
-		use("folke/zen-mode.nvim")
-		use({
-			"iamcco/markdown-preview.nvim",
-			run = function()
-				vim.fn["mkdp#util#install"]()
-			end,
-		})
-		use("akinsho/nvim-bufferline.lua")
-		-- use 'github/copilot.vim'
+        use("nvimdev/lspsaga.nvim") -- LSP UIs
+        use("L3MON4D3/LuaSnip")
+        use({
+            "nvim-treesitter/nvim-treesitter",
+            run = function()
+                require("nvim-treesitter.install").update({
+                    with_sync = true,
+                })
+            end,
+        })
+        use("kyazdani42/nvim-web-devicons") -- File icons
+        use("nvim-telescope/telescope.nvim")
+        use("nvim-telescope/telescope-file-browser.nvim")
+        use("windwp/nvim-autopairs")
+        use("windwp/nvim-ts-autotag")
+        use({
+            "numToStr/Comment.nvim",
+            requires = { "JoosepAlviste/nvim-ts-context-commentstring" },
+        })
+        use("norcalli/nvim-colorizer.lua")
+        use("folke/zen-mode.nvim")
+        use({
+            "iamcco/markdown-preview.nvim",
+            run = function()
+                vim.fn["mkdp#util#install"]()
+            end,
+        })
+        use("akinsho/nvim-bufferline.lua")
+        -- use 'github/copilot.vim'
 
-		use("lewis6991/gitsigns.nvim")
-		use("dinhhuy258/git.nvim") -- For git blame & browse
+        use("lewis6991/gitsigns.nvim")
+        use("dinhhuy258/git.nvim") -- For git blame & browse
 
-		use({
-			"kylechui/nvim-surround",
-			tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-			config = function()
-				require("nvim-surround").setup({
-					-- Configuration here, or leave empty to use defaults
-				})
-			end,
-		})
-		use({
-			"nvim-tree/nvim-tree.lua",
-			requires = {
-				"nvim-tree/nvim-web-devicons", -- optional
-			},
-			config = function()
-				require("nvim-tree").setup({})
-			end,
-		})
-		-- Vim configuration for Rust
-		--	use({
-		--		"rust-lang/rust.vim",
-		--		ft = { "rust" },
-		--		config = function()
-		--			require("plugins.rust")
-		--		end,
-		--	})
-		use({
-			"jackMort/ChatGPT.nvim",
-			event = { "CmdlineEnter" },
-			requires = {
-				{ "MunifTanjim/nui.nvim", opt = true },
-				{ "nvim-lua/plenary.nvim", opt = true },
-				{ "nvim-telescope/telescope.nvim", opt = true },
-			},
-			wants = { "nui.nvim", "plenary.nvim", "telescope.nvim" },
-			config = function()
-				require("chatgpt").setup()
-			end,
-		})
-		use({
-			"folke/which-key.nvim",
-			config = function()
-				vim.o.timeout = true
-				vim.o.timeoutlen = 300
-				require("which-key").setup({
-					-- your configuration comes here
-					-- or leave it empty to use the default settings
-					-- refer to the configuration section below
-				})
-			end,
-		})
-		use({
-			"dstein64/vim-startuptime",
-			event = { "CmdlineEnter" },
-		}) -- Measure startup time
-	end)
+        use({
+            "kylechui/nvim-surround",
+            tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+            config = function()
+                require("nvim-surround").setup({
+                    -- Configuration here, or leave empty to use defaults
+                })
+            end,
+        })
+        use({
+            "nvim-tree/nvim-tree.lua",
+            requires = {
+                "nvim-tree/nvim-web-devicons", -- optional
+            },
+            config = function()
+                require("nvim-tree").setup({})
+            end,
+        })
+        -- Vim configuration for Rust
+        --	use({
+        --		"rust-lang/rust.vim",
+        --		ft = { "rust" },
+        --		config = function()
+        --			require("plugins.rust")
+        --		end,
+        --	})
+        use({
+            "jackMort/ChatGPT.nvim",
+            event = { "CmdlineEnter" },
+            requires = {
+                { "MunifTanjim/nui.nvim",          opt = true },
+                { "nvim-lua/plenary.nvim",         opt = true },
+                { "nvim-telescope/telescope.nvim", opt = true },
+            },
+            wants = { "nui.nvim", "plenary.nvim", "telescope.nvim" },
+            config = function()
+                require("chatgpt").setup()
+            end,
+        })
+        use({
+            "folke/which-key.nvim",
+            config = function()
+                vim.o.timeout = true
+                vim.o.timeoutlen = 300
+                require("which-key").setup({
+                    -- your configuration comes here
+                    -- or leave it empty to use the default settings
+                    -- refer to the configuration section below
+                })
+            end,
+        })
+        use({
+            "dstein64/vim-startuptime",
+            event = { "CmdlineEnter" },
+        }) -- Measure startup time
+        use("craftzdog/solarized-osaka.nvim")
+    end)
 end
