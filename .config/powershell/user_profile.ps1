ee4c0a3f6be18cf0126e31a18bb1467de9bf48aa
Yoshinori Kashimura <yoshinori-kashimura@hldc.co.jp>
2024-06-21 09:10:28 +0900

    rm,rmdirでファイル及びフォルダをゴミ箱に移動するコマンドを追加した

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,7 +1,8 @@
 # Prompt
 Import-Module posh-git
 # Set-PoshPrompt Paradox
-if ($env:WT_PROFILE_ID) {
+if ($env:WT_PROFILE_ID)
+{
     # Windows Terminalから実行されたときだけ変更する設定をここに記述する
     # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" | Invoke-Expression
     # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\slimfat.omp.json" | Invoke-Expression
@@ -33,54 +34,92 @@ Set-Alias fb fzf-bat
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias gs git-status
+Set-Alias rm File-ToRecycleBin
+Set-Alias rmdir Folder-ToRecycleBin
 
 # Function
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+function which ($command)
+{
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+        Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-function fzf-bat {
-  fzf --reverse --inline-info --ansi --preview "bat --color=always --style=header,grid --line-range :100 {}"
+function fzf-bat
+{
+    fzf --reverse --inline-info --ansi --preview "bat --color=always --style=header,grid --line-range :100 {}"
 }
 
-function pwdc {
-  pwd | Set-Clipboard
+function pwdc
+{
+    pwd | Set-Clipboard
 }
 
-function opex {
-  param([string]$path = ".")
-  Invoke-Item -Path $path
+function opex
+{
+    param([string]$path = ".")
+    Invoke-Item -Path $path
 }
 
-function pathc ($path) {
-  Get-Item $path | Select-Object -ExpandProperty FullName | Set-Clipboard
+function pathc ($path)
+{
+    Get-Item $path | Select-Object -ExpandProperty FullName | Set-Clipboard
 }
 
-function merge ($path1, $path2) {
+function merge ($path1, $path2)
+{
     & "C:\Program Files\Araxis\Araxis Merge\Merge.exe" $path1 $path2
 }
 
-function treec {
-   tree /f | Set-Clipboard
+function treec
+{
+    tree /f | Set-Clipboard
 }
 
-function touch ($name) {
+function touch ($name)
+{
     New-Item -ItemType File $name
 }
 
-function .. {
+function ..
+{
     cd ..
 }
 
-function ... {
+function ...
+{
     cd ..\..
 }
 
-function .... {
+function ....
+{
     cd ..\..\..
 }
 
-function git-status {
+function git-status
+{
     git status
+}
+
+function File-ToRecycleBin($target_file_path)
+{
+    if ((Test-Path $target_file_path) -And ((Test-Path -PathType Leaf (Get-Item $target_file_path))))
+    {
+        $fullpath = (Get-Item $target_file_path).FullName
+        [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile($fullpath,'OnlyErrorDialogs','SendToRecycleBin')
+    } else
+    {
+        Write-Output "'$target_file_path' is not file or not found."
+    }
+}
+
+function Folder-ToRecycleBin($target_dir_path)
+{
+    if ((Test-Path $target_dir_path) -And ((Test-Path -PathType Container (Get-Item $target_dir_path))))
+    {
+        $fullpath = (Get-Item $target_dir_path).FullName
+        [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory($fullpath,'OnlyErrorDialogs','SendToRecycleBin')
+    } else
+    {
+        Write-Output "'$target_dir_path' is not directory or not found."
+    }
 }
