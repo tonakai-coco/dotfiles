2d700fa052017da9747f69806ecb2ef6f4106910
Yoshinori Kashimura <yoshinori-kashimura@hldc.co.jp>
2024-12-30 22:32:25 +0900

    cursorから読み出されるターミナルの設定を考慮し、読み出すModuleを調整した。

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,30 +1,31 @@
-# Prompt
-Import-Module posh-git
-# Set-PoshPrompt Paradox
 if ($env:WT_PROFILE_ID)
 {
     # Windows Terminalから実行されたときだけ変更する設定をここに記述する
+    # Prompt
+    # Set-PoshPrompt Paradox
+    Import-Module posh-git
     # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" | Invoke-Expression
     # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\slimfat.omp.json" | Invoke-Expression
     # oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gmay.omp.json" | Invoke-Expression
     oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\takuya.omp.json" | Invoke-Expression
+
+    # 文字コードをUTF-8に変更
+    chcp 65001
+
+    # Icons
+    Import-Module -Name Terminal-Icons
+    #
+    # Fzf
+    Import-Module PSFzf
+    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
 }
-
-# 文字コードをUTF-8に変更
-chcp 65001
-
-# Icons
-Import-Module -Name Terminal-Icons
 
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
-
-# Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # Alias
 Set-Alias vim nvim
