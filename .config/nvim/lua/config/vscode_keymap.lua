ac2739b6758675de5961389e264026cff19c5056
Yoshinori Kashimura <yoshinori-kashimura@hldc.co.jp>
2025-05-30 08:28:46 +0900

    VSCodeのGo to Fileコマンドのショートカットを追加した。

--- .config/nvim/lua/config/vscode_keymap.lua
+++ .config/nvim/lua/config/vscode_keymap.lua
@@ -25,12 +25,19 @@ keymap.set("v", "v", "<C-v>", {
   noremap = true,
 })
 
+-- VSCodeのキーバインドを設定
 keymap.set("n", "<Leader>w", "<cmd>call VSCodeNotify('workbench.action.files.save')<cr>", {
   noremap = true,
 })
+
 keymap.set("n", "<Leader>q", "<cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<cr>", {
   noremap = true,
 })
+
 keymap.set("n", "<Leader>e", "<cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<cr>", {
+  noremap = true,
+})
+
+keymap.set("n", "<Leader>ff", "<cmd>call VSCodeNotify('workbench.action.quickOpen')<cr>", {
   noremap = true,
 })
