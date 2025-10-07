939091dbdd57c9d57b3a19924c0b7d72f8e96c75
tonakai_coco <73034880+tonakai-coco@users.noreply.github.com>
2025-10-08 00:00:45 +0900

    feat: weztermのデザインを調整した

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,50 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+config.automatically_reload_config = true
+config.font_size = 12.0
+config.use_ime = true
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_frame = {
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+}
+
+config.window_background_gradient = {
+	colors = { "#000000" },
+}
+
+config.show_new_tab_button_in_tab_bar = false
+-- config.show_close_tab_button_in_tab_bar = false  -- ナイトリービルドのみのオプション
+config.colors = {
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
+}
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+
+	if tab.is_active then
+		background = "#ae8b2d"
+		foreground = "#FFFFFF"
+	end
+
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+	return {
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+	}
+end)
+
+-- and finally, return the configuration to wezterm
+return config
