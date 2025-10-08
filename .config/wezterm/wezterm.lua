5bc35b7ae3b5955bf39721bc4eb76926a3b525f5
tonakai_coco <73034880+tonakai-coco@users.noreply.github.com>
2025-10-08 13:39:34 +0900

    feat: icebergテーマを設定

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,10 +8,11 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.font_size = 12.5
 config.use_ime = true
-config.window_background_opacity = 0.85
-config.macos_window_background_blur = 20
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 30
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
+config.color_scheme = "iceberg-dark"
 
 config.window_frame = {
 	inactive_titlebar_bg = "none",
