5ebe5a2e96f33fb2affae2955fa8636c7f5d033c
tonakai-coco <73034880+tonakai-coco@users.noreply.github.com>
2026-04-21 09:09:07 +0900

    feat(nvim): ディレクトリ指定起動時に oil.nvim を自動表示
    nvim . のようにディレクトリを引数に起動した際、
    VimEnter autocmd で oil.nvim を自動的に開くよう設定した。
    ファイル指定や引数なし起動には影響しない。

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -3,6 +3,17 @@ return {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {},
+    init = function()
+      vim.api.nvim_create_autocmd("VimEnter", {
+        nested = true,
+        callback = function()
+          local bufname = vim.api.nvim_buf_get_name(0)
+          if vim.fn.isdirectory(bufname) == 1 then
+            require("oil").open(bufname)
+          end
+        end,
+      })
+    end,
     keys = {
       {
         "sf",
